60e10013482af35be082a26773a15161abe7efd0
Pellegrini-Jacopo <pellegrini.jacopo@iisgalvanimi.edu.it>
2022-10-26 10:58:02 +0000

    es5.py

--- es4.py
+++ es4.py
@@ -29,4 +29,35 @@ for x in range(6):
 for x in range(2, 30, 3):
   print(x)
 
+#abbino i nomi di un grtuppo con quelli dell'altro
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
 
+for x in adj:
+  for y in fruits:
+    print(x, y)
+
+#se metti past eviti errori da parte del sistem
+for x in [0, 1, 2]:
+  pass
+
+#Gli array vengono utilizzati per memorizzare più valori in una singola variabile:
+cars = ["Ford", "Volvo", "BMW"]
+
+#Ottieni il valore del primo elemento dell'array:
+x = cars[0]
+
+#Restituisce il numero di elementi cars nell'array:
+x = len(cars)
+
+#Stampa ogni elemento carsnell'array:
+for x in cars:
+  print(x)
+
+#Aggiungi un altro elemento cars all'array:
+cars.append("Honda")
+
+#Elimina il primo elemento carsdell'array:
+cars.pop(0)
+
+#
